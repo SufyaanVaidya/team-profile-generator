f93cefefe246cb67317f570991a60c606815bb2a
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-19 22:03:26 -0400

    fixed employee test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -2,8 +2,9 @@ const Employee = require('../lib/employee');
 
 describe('Employee class', () => {
     it('Collect user information for the employee', () => {
-        expect(new Employee(person.name)).toBe('Hector');
-        expect(new Employee(person.id)).toBe('1');
-        expect(new Employee(person.email)).toBe('hector@fakeemail.com');
+        expect(new Employee(person.name).getName('Hector'));
+        expect(new Employee(person.id).getId('1'));
+        expect(new Employee(person.email).getEmail('hector@fakeemail.com'));
+        expect(new Employee(person.role).getRole('Employee'));
     });
 });
