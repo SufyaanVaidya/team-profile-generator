8761811251550dba61283a5ebd0f136e6679a9c8
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-20 02:43:39 -0400

    added test for manager

---
+++ test/manager.test.js
@@ -0,0 +1,12 @@
+const { number } = require('yargs');
+const Manager = require('../lib/manager.js');
+
+test('Creates object for the manager', () => {
+    const manager = new Manager('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 7);
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('Collect the role of the intern', () => {
+    const manager = new Manager('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 7);
+    expect(manager.getRole()).toEqual('Intern');
+});
