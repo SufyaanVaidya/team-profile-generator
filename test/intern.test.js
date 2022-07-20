b4015c80eef83c7f1b36563e6a60742d1cdc4de2
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-20 02:38:45 -0400

    added intern.js and fixed errors

--- test/intern.test.js
+++ test/intern.test.js
@@ -2,13 +2,13 @@ const Intern = require('../lib/intern.js');
 
 test('Creates object for the intern', () => {
     const intern = new Intern('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'UCB');
-    expect(intern.github).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
 });
 test('Collect school for the intern', () => {
-    const intern = new Engineer('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'UCB');
+    const intern = new Intern('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'UCB');
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 test('Collect the role of the intern', () => {
-    const intern = new Engineer('Sufyaan', 90, 'vaidyasufyaan@gmail.com', "UCB");
+    const intern = new Intern('Sufyaan', 90, 'vaidyasufyaan@gmail.com', "UCB");
     expect(intern.getRole()).toEqual('Intern');
 });
