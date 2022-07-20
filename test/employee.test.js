e9ae8e95e3e8979c15efa9543a29cb5b34af7b16
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-19 23:24:31 -0400

    added the rest of employee test

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,13 +1,27 @@
-const { number } = require('yargs');
+const { default: test } = require('node:test');
 const Employee = require('../lib/employee.js');
 
-describe('Employee class', () => {
-    it('Collect user information for the employee', () => {
-        const person = new Employee('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'Manager')
+test('Collect user information for the employee', () => {
+        const person = new Employee('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'Manager');
         expect(person.name).toEqual(expect.any(String));
         expect(person.id).toEqual(expect.any(Number));
         expect(person.email).toEqual(expect.any(String));
         expect(person.role).toEqual(expect.any(String)); 
     });
     
+test('get the employees name', () => {
+    const person = new Employee('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'Manager');
+    expect(person.name).toEqual(expect.any(String));
+});
+test('get the employees id', () => {
+    const person = new Employee('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'Manager');
+    expect(person.id).toEqual(expect.any(Number));
+});
+test('get the employees email', () => {
+    const person = new Employee('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'Manager');
+    expect(person.email).toEqual(expect.any(String));
+});
+test('get the employees role', () => {
+    const person = new Employee('Sufyaan', 90, 'vaidyasufyaan@gmail.com', 'Manager');
+    expect(person.ole).toEqual(expect.any(String));
 });
